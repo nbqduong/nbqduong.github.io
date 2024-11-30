fe6acc716bb86697ffa39139cad0efd385270b85
nbqduong <86090755+nbqduong@users.noreply.github.com>
2024-11-30 18:05:09 +0700

    fix hero text after build

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -90,15 +90,7 @@ const About = () => {
             <Gradient />
             
           </div>
-          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
-            <img
-              src={heroBackground}
-              className="w-full"
-              width={1440}
-              height={1800}
-              alt="hero"
-            />
-          </div>
+
           
           
         </div>
